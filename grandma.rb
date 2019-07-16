b6db08f9717084198f873d7b5e8dd02d03631dd0
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-16 16:44:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,8 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
- elsif phrase == "Hi Nana, how"
+elsif phrase == phrase.upcase
+  "NO, NOT SINCE 1938!"
  
   end
 end
